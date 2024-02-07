e58e71a9a6ee9cedce0e041b2af5ddab4cfb6a8d
Fanuel <fanuelabebe@gmail.com>
2024-02-07 21:34:07 +0300

    Feat: Added Folder Structure and added UI Activities for both modules

---
+++ app/src/main/java/com/fan/locationsmartcontract/driver/ui/DriverActivity.java
@@ -0,0 +1,16 @@
+package com.fan.locationsmartcontract.driver.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fan.locationsmartcontract.R;
+
+public class DriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+    }
+}
